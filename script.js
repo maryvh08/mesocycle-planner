f90c7f002849558de92ccd297e1f7b0c3e401390
MaryVH08 <marycarmen.vives@aneiap.co>
2026-01-01 12:38:46 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
-document.getElementById("workout-form")
-  .addEventListener("submit", function (e) {
-    e.preventDefault();
-    alert("Más adelante guardaremos este entrenamiento 😉");
-  });
+const form = document.getElementById("workout-form");
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const inputs = form.querySelectorAll("input");
+
+  const { error } = await supabase
+    .from("workouts")
+    .insert([
+      {
+        exercise: inputs[0].value,
+        reps: inputs[1].value,
+        weight: inputs[2].value
+      }
+    ]);
+
+  if (error) {
+    alert("Error al guardar");
+    console.error(error);
+  } else {
+    alert("Entrenamiento guardado 💪");
+    form.reset();
+  }
+});
