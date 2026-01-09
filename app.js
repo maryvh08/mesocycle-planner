ffa1e91f22f5486f74757ecae8ccc02c0e782e96
MaryVH08 <marycarmen.vives@aneiap.co>
2026-01-08 22:29:05 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,13 +2,22 @@ import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 let activeMesocycle = null;
 const daySelect = document.getElementById("day-select");
 const exerciseConfig = document.getElementById("exercise-config");
-
-
 const SUPABASE_URL = "https://vhwfenefevzzksxrslkx.supabase.co";
 const SUPABASE_ANON_KEY =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZod2ZlbmVmZXZ6emtzeHJzbGt4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc5MTE3ODAsImV4cCI6MjA4MzQ4Nzc4MH0.CG1KzxpxGHifXsgBvH-4E4WvXbj6d-8WsagqaHAtVwo";
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+if (!activeMesocycle) {
+  alert("Selecciona un mesociclo primero");
+  return;
+}
+
+daySelect.onchange = () => {
+  exerciseConfig
+    .querySelectorAll("input")
+    .forEach(cb => cb.checked = false);
+};
 
 /* ======================
    AUTH UI REFERENCES
